4bee2ba6f77fd2419703fb26e1de0896b497d70d
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-06 18:36:22 -0300

    logica de escolher o arquivo que o parser vai ler

--- trabalho-tdd/patex/src/main/java/com/tppe/tdd/patex/App.java
+++ trabalho-tdd/patex/src/main/java/com/tppe/tdd/patex/App.java
@@ -1,5 +1,10 @@
 package com.tppe.tdd.patex;
 
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
 /**
  * Hello world!
  */
@@ -12,6 +17,18 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+        final JFileChooser fc = new JFileChooser();
+
+        fc.setApproveButtonText("Choose");
+        int approve = fc.showOpenDialog(null);
+
+        if(approve == fc.APPROVE_OPTION){
+            File chosenFile = fc.getSelectedFile();
+
+            JOptionPane.showMessageDialog(null, "Arquivo escolhido: \n" +
+                chosenFile.getAbsolutePath());
+
+        }
     }
 }
