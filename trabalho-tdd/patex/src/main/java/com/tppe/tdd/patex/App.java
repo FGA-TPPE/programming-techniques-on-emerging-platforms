657d88d10c0128d11cb872c6b226850a87eabe95
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-06 18:58:01 -0300

    usa a classe Patex para start e stop o app

--- trabalho-tdd/patex/src/main/java/com/tppe/tdd/patex/App.java
+++ trabalho-tdd/patex/src/main/java/com/tppe/tdd/patex/App.java
@@ -1,10 +1,4 @@
 package com.tppe.tdd.patex;
-
-import java.io.File;
-
-import javax.swing.JFileChooser;
-import javax.swing.JOptionPane;
-
 /**
  * Hello world!
  */
@@ -12,23 +6,10 @@ public final class App {
     private App() {
     }
 
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
     public static void main(String[] args) {
-        // System.out.println("Hello World!");
-        final JFileChooser fc = new JFileChooser();
+        Patex patex = new Patex();
 
-        fc.setApproveButtonText("Choose");
-        int approve = fc.showOpenDialog(null);
-
-        if(approve == fc.APPROVE_OPTION){
-            File chosenFile = fc.getSelectedFile();
-
-            JOptionPane.showMessageDialog(null, "Arquivo escolhido: \n" +
-                chosenFile.getAbsolutePath());
-
-        }
+        patex.start();
+        patex.stop();
     }
 }
