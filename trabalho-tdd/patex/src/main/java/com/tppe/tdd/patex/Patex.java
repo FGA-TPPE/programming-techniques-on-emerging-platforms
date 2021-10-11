0435a6bcb3deb823fedecd5aaf52f944ada8cc13
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-11 17:41:29 -0300

    cria metodo para escolher arquvo e trata erro

--- trabalho-tdd/patex/src/main/java/com/tppe/tdd/patex/Patex.java
+++ trabalho-tdd/patex/src/main/java/com/tppe/tdd/patex/Patex.java
@@ -1,28 +1,40 @@
 package com.tppe.tdd.patex;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
 public class Patex {
     private JFileChooser fc;
+    File chosenFile;
 
     Patex(){
         this.fc = new JFileChooser();
+        this.chosenFile = null;
     }
 
-    void start(){
+    void chooseFile() throws FileNotFoundException{
         this.fc.setDialogTitle("Choose the file you want to parse");
         this.fc.setApproveButtonText("Choose");
         int approve = this.fc.showOpenDialog(null);
-
         if(approve == this.fc.APPROVE_OPTION){
-            File chosenFile = this.fc.getSelectedFile();
+            this.chosenFile = this.fc.getSelectedFile();
+            // JOptionPane.showMessageDialog(null, "Chosen file: \n" +
+            //     chosenFile.getAbsolutePath());
+        }
+        else
+            throw new FileNotFoundException("No file was chosen");
+    }
 
-            JOptionPane.showMessageDialog(null, "Chosen file: \n" +
-                chosenFile.getAbsolutePath());
-
+    void start(){
+        try {
+            this.chooseFile();
+        } catch (FileNotFoundException e){
+            JOptionPane.showMessageDialog(null, "You must choose a file to continue");
+        } catch (Exception e) {
+            System.out.println("Unexpected exception occurred");
         }
     }
 
