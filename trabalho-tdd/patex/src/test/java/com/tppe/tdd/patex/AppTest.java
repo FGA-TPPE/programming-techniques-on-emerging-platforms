dd34d928aac25e35c844b0a8fa1edfd04de94c47
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-11 17:42:07 -0300

    tdd: testa escolha do arquivo

--- trabalho-tdd/patex/src/test/java/com/tppe/tdd/patex/AppTest.java
+++ trabalho-tdd/patex/src/test/java/com/tppe/tdd/patex/AppTest.java
@@ -2,17 +2,24 @@ package com.tppe.tdd.patex;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.FileNotFoundException;
 
 /**
  * Unit test for simple App.
  */
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
-    @Test
-    void testApp() {
-        assertEquals(1, 1);
+
+    private Patex patexapp;
+
+    AppTest(){
+        this.patexapp = new Patex();
+    }
+
+    @Test()
+    void testFileChoosing() throws FileNotFoundException {
+        this.patexapp.chooseFile();
+        assertNotNull(this.patexapp.chosenFile);
     }
 }
