a92cb79aa6d923224aafd0e4c09cbcd643223ed1
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-13 08:58:56 -0300

    inicializa o delimiter no construtor

--- trabalho-tdd/patex/src/main/java/com/tppe/tdd/patex/Patex.java
+++ trabalho-tdd/patex/src/main/java/com/tppe/tdd/patex/Patex.java
@@ -11,11 +11,12 @@ import com.tppe.tdd.patex.Exceptions.DelimitadorInvalidoException;
 public class Patex {
     private JFileChooser fc;
     File chosenFile;
-    String delimiter = null;
+    String delimiter;
 
     Patex(){
         this.fc = new JFileChooser();
         this.chosenFile = null;
+        this.delimiter = null;
     }
 
     Patex(String pathToFile){
@@ -47,8 +48,8 @@ public class Patex {
     void chooseDelimiter() throws DelimitadorInvalidoException{
         String result = JOptionPane.showInputDialog("Selecione o Delimitador utilizado entre os valores");
         if( result.length()>2 ||
-            result.length() == 2 && !result.startsWith("/") 
-        )    
+            result.length() == 2 && !result.startsWith("/")
+        )
             throw new DelimitadorInvalidoException("O Delimitador deve ser um caracter ou deve estar precedido do '/' ");
             this.delimiter = result;
     }
