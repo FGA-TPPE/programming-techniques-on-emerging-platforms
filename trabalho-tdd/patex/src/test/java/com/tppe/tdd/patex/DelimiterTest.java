2835eeb49dbd3425fe8725622ae58b1f8e578cc1
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-15 09:06:16 -0300

    Merge branch 'dev' into 'dev'
    Please enter the commit message for your changes. Lines starting

--- trabalho-tdd/patex/src/test/java/com/tppe/tdd/patex/DelimiterTest.java
+++ trabalho-tdd/patex/src/test/java/com/tppe/tdd/patex/DelimiterTest.java
@@ -6,41 +6,41 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import com.tppe.tdd.patex.Exceptions.DelimitadorInvalidoException;
 
 public class DelimiterTest {
-    
+
     private Patex patexapp;
 
     public DelimiterTest() {
         this.patexapp = new Patex();
     }
-    
+
     @Test()
     void testDelimiterChoosing(){
-        try{    
+        try{
             this.patexapp.chooseDelimiter();
             assertEquals(";",patexapp.delimiter);
-        }catch(Exception e){}    
+        }catch(Exception e){}
     }
 
     @Test()
     void testDelimiterChoosing2(){
         try{this.patexapp.chooseDelimiter();
             assertEquals(",",patexapp.delimiter);
-        }catch(Exception e) {}   
+        }catch(Exception e) {}
     }
-    
+
     @Test()
-    void testDelimiterString(){    
+    void testDelimiterString(){
         assertThrows(DelimitadorInvalidoException.class, () ->
             this.patexapp.chooseDelimiter()
         );
     }
 
     @Test()
-    void testDelimiterEscape(){    
+    void testDelimiterEscape(){
         try{this.patexapp.chooseDelimiter();
-            assertEquals("/n",patexapp.delimiter);
-        }catch(Exception e) {}  
-    
+            assertEquals("\\n",patexapp.delimiter);
+        }catch(Exception e) {}
+
     }
 
 }
