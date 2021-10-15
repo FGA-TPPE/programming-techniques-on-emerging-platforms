8642ed8f035b4a26d0736205d4c84d7e4b2c11d4
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-15 09:12:31 -0300

    tdd-falsificacao: testa o usuario escolhendo o formato de saida (linha ou coluna)

---
+++ trabalho-tdd/patex/src/test/java/com/tppe/tdd/patex/OutputFormatTest.java
@@ -0,0 +1,16 @@
+package com.tppe.tdd.patex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class OutputFormatTest extends AppTest {
+    OutputFormatTest(){
+        super();
+    }
+
+    @Test()
+    void testOutputFormatChoosing(){
+        assertEquals(true, this.patexapp.choseOutputFormat());
+    }
+}
