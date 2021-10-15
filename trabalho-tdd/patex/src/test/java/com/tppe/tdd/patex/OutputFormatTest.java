21b996ead5f335ec3929d10500c25ac0ebeda875
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-15 11:19:43 -0300

    tdd-duplicacao: duplica o teste de escolha do formato de saida

--- trabalho-tdd/patex/src/test/java/com/tppe/tdd/patex/OutputFormatTest.java
+++ trabalho-tdd/patex/src/test/java/com/tppe/tdd/patex/OutputFormatTest.java
@@ -1,6 +1,7 @@
 package com.tppe.tdd.patex;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -12,5 +13,12 @@ public class OutputFormatTest extends AppTest {
     @Test()
     void testOutputFormatChoosing() throws Exception{
         assertEquals(true, this.patexapp.choseOutputFormat());
+    }
+
+    @Test()
+    void testWrongOutputFormatChoosing() throws Exception {
+        /* Since the method being tested needs user interaction, for this test
+        to pass you need to hit the cancel or the 'x' button */
+        assertThrows(Exception.class, () -> this.patexapp.choseOutputFormat());
     }
 }
