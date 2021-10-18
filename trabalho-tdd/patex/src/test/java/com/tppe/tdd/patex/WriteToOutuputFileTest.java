d23813637487d5616b0f8c668e8d1e39c7714380
ErickGiffoni <giffoni.erick@gmail.com>
2021-10-18 10:18:09 -0300

    tdd: testa escrita falsificada no arquivo de saida

---
+++ trabalho-tdd/patex/src/test/java/com/tppe/tdd/patex/WriteToOutuputFileTest.java
@@ -0,0 +1,22 @@
+package com.tppe.tdd.patex;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.File;
+
+import org.junit.jupiter.api.Test;
+
+public class WriteToOutuputFileTest extends ChooseFile {
+    private File testFile;
+
+    WriteToOutuputFileTest(){
+        super();
+        this.testFile = new File("../outputTest/testFile.txt");
+    }
+
+    @Test()
+    void testWriteToOutputFile() {
+        this.patexapp.outputPath = this.testFile.getAbsolutePath();
+        assertEquals(true, this.patexapp.writeToOutputFile());
+    }
+}
